2678d7c1b86ba0ad895d8376693137d92b4fa21e
Fiona K. Ewald <57101697+fionawald@users.noreply.github.com>
2023-08-25 07:55:15 +0200

    add_sqrt_scale

--- R/sim1_plot.R
+++ R/sim1_plot.R
@@ -4,7 +4,13 @@ library("mlr3verse")
 library("ggplot2")
 library("iml")
 library("dplyr")
+library("scales")
 theme_set(theme_bw())
+
+# squared scale with negative values
+S_sqrt <- function(x){sign(x)*sqrt(abs(x))}
+IS_sqrt <- function(x){x^2*sign(x)}
+S_sqrt_trans <- function() trans_new("S_sqrt",S_sqrt,IS_sqrt)
 
 set.seed(123)
 
@@ -33,7 +39,8 @@ p = ggplot(data=df, aes(x=reorder(type, X), y=importance, fill=reorder(feature, 
   geom_errorbar(aes(ymin=q.05, ymax=q.95), width=.2, position=position_dodge(.9))
 
 p = p + labs(x='IML technique', y='importance', fill='feature')
-p + coord_flip()+
+# delete scale_y_continuous in the following if you want "normal" scales
+p + coord_flip() + scale_y_continuous(trans="S_sqrt",breaks=seq(-0.1,0.5,0.05))+
   scale_fill_discrete(breaks=c('x1', 'x2', 'x3', 'x4', 'x5'))
 
 ggsave('../figures/cfi_pfi_SAGEvalueFunc_orig.pdf', width=6, height=5)
